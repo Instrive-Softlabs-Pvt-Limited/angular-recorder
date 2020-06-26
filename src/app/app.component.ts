77fdb7f683278fc2bcef0db1877b442fa1bf83b2
seagull <ys.madhu@gmail.com>
2020-06-27 00:05:36 +0900

    version1

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  format = 'video/webm';
+  constrains = {video: true, audio: true};
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  public handleVideoStream(blob) {
+    // can send it to a server or play in another video player etc..
+    console.log('do something with the recording' + blob);
+  }
+
+  announceStart() {
+    alert('Start recording!');
+  }
+}
